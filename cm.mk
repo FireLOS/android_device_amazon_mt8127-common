42f6a4c85a2a3e6a354a59b212e9347781be136a
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2020-04-17 21:15:25 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,1 +1,3 @@
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product-if-exists, vendor/microg/microg.mk)
+$(call inherit-product-if-exists, vendor/apps/apps.mk)
