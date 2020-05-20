0b865f529edb2df89dba76045a05045934a6a5ef
ANDROID2468 <39718118+ANDROID2468@users.noreply.github.com>
2020-05-20 02:38:11 -0500

    added bromite browser

--- cm.mk
+++ cm.mk
@@ -1,3 +1,2 @@
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
-$(call inherit-product-if-exists, vendor/microg/microg.mk)
-$(call inherit-product-if-exists, vendor/apps/apps.mk)
+$(call inherit-product-if-exists, vendor/cm/config/32_apps.mk)
